8b731e87dc838ad4a6951b819a85efa85126058d
Sue <susanhartesquire@yahoo.com>
2019-10-17 12:22:34 -0400

    commit changed files

--- app.js
+++ app.js
@@ -3,12 +3,68 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const Sequelize = require("sequelize"); //importing sequelize
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
 // create the Express app
 const app = express();
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./fsjstd-restapi.db"
+});
+
+//Define Your Sequelize Models
+
+class User extends Sequelize.Model {} //created book class
+User.init(
+  {
+    //initializes the model object
+    id: {
+      type: Sequelize.INTEGER, 
+      primaryKey: true, //properties of the user  
+    },
+    firstName: {
+      type: Sequelize.STRING,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+    },
+    emailAddress: Sequelize.STRING, //key, value
+    password: Sequelize.STRING
+  },
+  { sequelize, modelName: "user" }
+); 
+
+class Course extends Sequelize.Model {} //created Course class
+Course.init(
+  {
+    //initializes the model object
+    id: {
+      type: Sequelize.INTEGER, 
+      primaryKey: true, //properties of the user
+    },
+    userId: { //user ID references the User model and uses the id key, this is called a one-to-one relationship
+      references: {
+        model: User,
+        key: 'id',
+      }},
+      title: {
+         type: Sequelize.STRING, //properties of the book
+      },
+      description: {
+      type: Sequelize.TEXT,
+      },
+      estimatedTime: Sequelize.STRING, //key, value
+      materialsNeeded: Sequelize.STRING
+  },
+  { sequelize, modelName: "course" }
+); 
+
+sequelize.authenticate().then(function(err) {
+  console.log('Connection successful');
+})
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
