d6e60e4c51a444bc843b645b945184a0dc3dde1a
Sue <susanhartesquire@yahoo.com>
2019-10-29 16:41:04 -0400

    commit authentication function

--- app.js
+++ app.js
@@ -19,41 +19,41 @@ const Sequelize = require("sequelize"); //importing sequelize
 //  * @param {Response} res - The Express Response object.
 //  * @param {Function} next - The function to call to pass execution to the next middleware.
 //  */
-// const authenticateUser = (req, res, next) => {
-//   let message = null;
+const authenticateUser = (req, res, next) => {
+  let message = null;
 
-//   // Get the user's credentials from the Authorization header.
-//   const credentials = auth(req);
+  // Get the user's credentials from the Authorization header.
+  const credentials = auth(req);
 
-//   if (credentials) {
-//     // Look for a user whose `username` matches the credentials `name` property.
-//     const user = users.find(u => u.username === credentials.name);
+  if (credentials) {
+    // Look for a user whose `username` matches the credentials `name` property.
+    const user = users.find(u => u.username === credentials.name);
 
-//     if (user) {
-//       const authenticated = bcryptjs
-//         .compareSync(credentials.pass, user.password);
-//       if (authenticated) {
-//         console.log(`Authentication successful for username: ${user.username}`);
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        console.log(`Authentication successful for username: ${user.username}`);
 
-//         // Store the user on the Request object.
-//         req.currentUser = user;
-//       } else {
-//         message = `Authentication failure for username: ${user.username}`;
-//       }
-//     } else {
-//       message = `User not found for username: ${credentials.name}`;
-//     }
-//   } else {
-//     message = 'Auth header not found';
-//   }
+        // Store the user on the Request object.
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
 
-//   if (message) {
-//     console.warn(message);
-//     res.status(401).json({ message: 'Access Denied' });
-//   } else {
-//     next();
-//   }
-// };
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    next();
+  }
+};
 
 // // Construct a router instance.
 // const router = express.Router();
